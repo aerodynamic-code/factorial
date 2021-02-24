58e1596df869d2cc46279bac68e005f9f14d2d51
acameron-spu <74923289+acameron-spu@users.noreply.github.com>
2021-02-24 12:04:25 -0800

    Implemented Fibonacci recursive function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+using namespace std;
+
+int fib( int n) {
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    return fib(n-1) + fib (n-2);
+
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    int m;
+    int n;
+    n = 10;
+    m = fib ( n);
+    cout << "fib(" << n << ")= " << m << endl;
     std::cout << "Bye for now." << std::endl;
     return 0;
 }
